3734c49f0aaf5c07e4a452b4f3a6b1b5767677be
lekealex <alexmagalhaesny@gmail.com>
2023-08-14 12:07:32 -0300

    fotografia  2

---
+++ src/App.vue
@@ -0,0 +1,183 @@
+<template>
+  <header class="sticky-top site-header">
+    <nav class="navbar">
+      <div class="logo">Meu Site</div>
+      <ul class="nav-links">
+        <li><a href="#">Página Inicial</a></li>
+        <li><a href="#">Sobre Nós</a></li>
+        <li><a href="#">Serviços</a></li>
+        <li><a href="#">Contato</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <div id="carouselExampleAutoplaying" class="carousel main" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="a.jpeg" class="d-block w-100" alt="" />
+        </div>
+        <div class="carousel-item">
+          <img src="download.jpeg" class="d-block w-100" alt="" />
+        </div>
+        <div class="carousel-item">
+          <img src="images.jpeg" class="d-block w-100" alt="" />
+        </div>
+      </div>
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleAutoplaying"
+        data-bs-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleAutoplaying"
+        data-bs-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    <div class="container">
+      <!-- Divs adicionais -->
+      <div class="additional-container">
+        <div class="additional-item"><img src="download.jpeg" alt="" /></div>
+        <div class="additional-item"><img src="download.jpeg" alt="" /></div>
+        <div class="additional-item"><img src="download.jpeg" alt="" /></div>
+      </div>
+
+      <!-- Mais três divs abaixo das divs criadas -->
+      <div class="bottom-container">
+        <div class="bottom-item">
+          <img src="download.jpeg" alt="" />
+          <span class="bottom-text">kfjaskjdfsajdkfjska</span>
+        </div>
+        <div class="bottom-item"><img src="download.jpeg" alt="" /></div>
+        <div class="bottom-item"><img src="download.jpeg" alt="" /></div>
+      </div>
+      <div class="xd-container">
+        <div class="bottom-i"><img src="download.jpeg" alt="" /></div>
+        <div class="bottom-i"><img src="download.jpeg" alt="" /></div>
+        <div class="bottom-i"><img src="download.jpeg" alt="" /></div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.container {
+  background-color: #012345;
+  max-width: none;
+  width: 100%;
+}
+/* Estilos do cabeçalho */
+
+.site-header {
+  background-color: #012345;
+  color: #fff;
+  padding: 20px 0;
+  width: 100%;
+  left: 0;
+}
+
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+  width: 100%;
+}
+
+.logo {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.nav-links {
+  list-style: none;
+  display: flex;
+  gap: 20px;
+}
+
+.nav-links li a {
+  text-decoration: none;
+  color: #fff;
+  transition: color 0.3s ease;
+}
+
+.nav-links li a:hover {
+  color: #ff9900;
+}
+
+/* Estilos para a imagem */
+.image-container {
+  text-align: center;
+  margin-top: 50px;
+  border: 1px solid #ec0707; /* Adicionando a borda */
+  padding: 10px; /* Espaçamento interno da borda */
+}
+
+.image-container img {
+  max-width: 100%;
+}
+
+/* Estilos para as divs adicionais */
+.additional-container {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+}
+
+.additional-item {
+  width: calc(33.33% - 10px); /* Largura dividida em três com espaçamento entre as divs */
+  border: 1px solid #012345;
+  padding: 10px;
+  text-align: center;
+}
+
+/* Estilos para as divs no fundo */
+.bottom-container {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+  /* width: 200px; */
+}
+
+.bottom-item {
+  /* width: calc(33.33% - 10px); Largura dividida em três com espaçamento entre as divs */
+  width: 220px;
+  border: 1px solid #012345;
+  padding: 10px;
+  text-align: center;
+  overflow: hidden;
+  display: flex;
+}
+.bottom-item:hover {
+  /* width: calc(33.33% - 10px); Largura dividida em três com espaçamento entre as divs */
+  width: 300px;
+}
+.bottom-item img {
+  width: 220px;
+}
+.bottom-item .bottom-text {
+  color: white;
+}
+
+.xd-container {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+}
+.bottom-i {
+  width: calc(33.33% - 10px); /* Largura dividida em três com espaçamento entre as divs */
+  border: 1px solid #012345;
+  padding: 10px;
+  text-align: center;
+}
+</style>

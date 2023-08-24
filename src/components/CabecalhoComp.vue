b8845ab23f683ccd4284f74c76289ab7d52f5d5f
lekealex <106382948+lekealex@users.noreply.github.com>
2023-08-23 22:28:12 -0300

    commit

--- src/components/CabecalhoComp.vue
+++ src/components/CabecalhoComp.vue
@@ -1,56 +1,84 @@
 <template>
-    <header class="sticky-top site-header">
-    <nav class="navbar">
-      <div class="logo">Meu Site</div>
-      <ul class="nav-links">
-        <li><RouterLink to="/">Página Inicial</RouterLink></li>
-        <li><RouterLink to="/sobre">Sobre Nós</RouterLink></li>
-        <li><RouterLink to="/servico">Serviços</RouterLink></li>
-        <li><RouterLink to="/contato">Contato</RouterLink></li>
-      </ul>
-    </nav>
+  <header class="sticky-top site-header">
+      <nav class="navbar">
+          <div class="logo">Fotographx</div>
+          <ul class="nav-links">
+              <li><RouterLink to="/">Página Inicial</RouterLink></li>
+              <li><RouterLink to="/sobre">Sobre Nós</RouterLink></li>
+              <li><RouterLink to="/servico">Serviços</RouterLink></li>
+              <li><RouterLink to="/contato">Contato</RouterLink></li>
+          </ul>
+      </nav>
   </header>
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap');
+
+* {
+font-family: 'Raleway', sans-serif;
+}
 
 .site-header {
-  background-color: #012345;
-  color: #fff;
-  padding: 20px 0;
-  width: 100%;
-  left: 0;
+color: #ccd5ae;
+padding: 20px 0;
+width: 100%;
+left: 0;
 }
 
 .navbar {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 0 20px;
-  width: 100%;
+display: flex;
+justify-content: space-between;
+align-items: center;
+max-width: 1200px;
+margin: 0 auto;
+padding: 0 20px;
+width: 100%;
+color: #ccd5ae;
 }
 
 .logo {
-  font-size: 24px;
-  font-weight: bold;
+font-size: 24px;
+font-weight: bold;
 }
 
 .nav-links {
-  list-style: none;
-  display: flex;
-  gap: 20px;
+list-style: none;
+display: flex;
+gap: 20px;
+font-weight: 600;
+}
+
+.nav-links li {
+position: relative;
 }
 
 .nav-links li a {
-  text-decoration: none;
-  color: #fff;
-  transition: color 0.3s ease;
+text-decoration: none;
+color: #ccd5ae;
+padding: 5px 10px; 
+transition: color 0.3s ease;
+display: flex; 
+align-items: center;
+}
+
+.nav-links li a::before {
+content: "";
+position: absolute;
+bottom: 0;
+left: 0;
+right: 0;
+height: 3px;
+background-color: #d4a373;
+transform: scaleY(0);
+transition: transform 0.3s ease;
 }
 
 .nav-links li a:hover {
-  color: #ff9900;
+color: #d4a373;
 }
 
+.nav-links li a:hover::before {
+transform: scaleY(1); 
+}
 </style>

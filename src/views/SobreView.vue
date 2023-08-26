38a3a23f227b2ac678968ca6df15bfaec62f2934
lekealex <106382948+lekealex@users.noreply.github.com>
2023-08-26 19:42:10 -0300

    Update SobreView.vue

--- src/views/SobreView.vue
+++ src/views/SobreView.vue
@@ -1,5 +1,6 @@
 <script setup>
   import FuncionarioComp from "@/components/FuncionarioComp.vue";
+  import TemasComp from "@/components/TemasComp.vue";
 </script>
 
 <template>
@@ -7,96 +8,19 @@
     <section class="about-section">
       <h1>Sobre Fotographx</h1>
       <p class="about-intro">Bem-vindo à Fotographx, a sua escolha confiável para capturar momentos inesquecíveis. Especializados em diversos temas, exploramos a beleza e a emoção através da lente.</p>
-
-      <!-- Casamentos -->
-      <div class="theme-container">
-        <div class="text-content">
-          <h2>Tema: Casamentos</h2>
-          <p>Com uma abordagem sensível e artística, capturamos o amor e a alegria dos casamentos, criando memórias que duram uma vida inteira.</p>
-        </div>
-        <img src="@/assets/imagens/1.jpg" alt="Casamentos" />
-      </div>
-
-      <!-- Retratos -->
-      <div class="theme-container reverse">
-        <div class="text-content">
-          <h2>Tema: Retratos</h2>
-          <p>De retratos individuais a fotos em família, nossa equipe habilidosa capta a essência de quem você é, refletindo personalidade e emoção.</p>
-        </div>
-        <img src="@/assets/imagens/2.jpg" alt="Retratos" />
-      </div>
-
-      <!-- Natureza -->
-      <div class="theme-container">
-        <div class="text-content">
-          <h2>Tema: Natureza</h2>
-          <p>Imortalizamos a beleza indomável e a tranquilidade da natureza, da fauna à flora.</p>
-        </div>
-        <img src="@/assets/imagens/1.jpg" alt="Natureza" />
-      </div>
-
-      <!-- Eventos Corporativos -->
-      <div class="theme-container reverse">
-        <div class="text-content">
-          <h2>Tema: Eventos Corporativos</h2>
-          <p>Com uma abordagem profissional, capturamos os momentos-chave dos seus eventos corporativos.</p>
-        </div>
-        <img src="@/assets/imagens/2.jpg" alt="Eventos Corporativos" />
-      </div>
-
+      <TemasComp />
     </section>
-    <section class="professionals-section">
-      <h2>Nossos Profissionais</h2>
-      <div class="professionals-grid">
-        <!-- Profissional 1 -->
-        <div class="professional-card">
-          <FuncionarioComp />
-          <h3>Nome do Profissional 1</h3>
-          <p>Especializado em fotografia de casamento</p>
-        </div>
-
-        <!-- Profissional 2 -->
-        <div class="professional-card">
-          <FuncionarioComp />
-          <h3>Nome do Profissional 2</h3>
-          <p>Especializado em retratos e fotografia de família</p>
-        </div>
-
-        <div class="professional-card">
-          <FuncionarioComp />
-          <h3>Nome do Profissional 3</h3>
-          <p>Especializado em fotografia de natureza</p>
-        </div>
-
-        <!-- Profissional 4 -->
-        <div class="professional-card">
-          <FuncionarioComp />
-          <h3>Nome do Profissional 4</h3>
-          <p>Especializado em fotografia de eventos corporativos</p>
-        </div>
-
-        <!-- Profissional 5 -->
-        <div class="professional-card">
-          <FuncionarioComp />
-          <h3>Nome do Profissional 5</h3>
-          <p>Especializado em fotografia de paisagens urbanas</p>
-        </div>
-
-        <!-- Profissional 6 -->
-        <div class="professional-card">
-          <FuncionarioComp />
-          <h3>Nome do Profissional 6</h3>
-          <p>Especializado em fotografia de comida</p>
-        </div>
-
-        <!-- Adicione mais profissionais conforme necessário -->
-      </div>
-    </section>
+    <FuncionarioComp />
 
   </main>
   <RouterView />
 </template>
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap');
+
+* {
+font-family: 'Raleway', sans-serif;
+}
 .about-section {
   padding: 20px;
   max-width: 1200px;

550f23083f5eb7264f45b92d26fbb45503035e49
lekealex <106382948+lekealex@users.noreply.github.com>
2023-08-26 19:41:57 -0300

    Update ServicoView.vue

--- src/views/ServicoView.vue
+++ src/views/ServicoView.vue
@@ -1,4 +1,19 @@
+<script setup>
+import ServicosComp from '../components/ServicosComp.vue';
+</script>
+
 <template>
-    <main>Serviços</main>
-    <RouterView />
-  </template>
+  <ServicosComp />
+  <RouterView />
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap');
+
+* {
+font-family: 'Raleway', sans-serif;
+}
+
+</style>
+
+

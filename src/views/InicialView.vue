d0d595e4ff35ae93e9b31f64b64761e9ab778b4e
lekealex <106382948+lekealex@users.noreply.github.com>
2023-09-05 12:58:03 -0300

    Update InicialView.vue

--- src/views/InicialView.vue
+++ src/views/InicialView.vue
@@ -1,9 +1,14 @@
 <script setup>
-  import ImagemComp from "@/components/ImagemComp.vue";
+import ImagemComp from "@/components/ImagemComp.vue";
+import CabecalhoComp from "../components/CabecalhoComp.vue";
+import FooterComp from "../components/FooterComp.vue";
 </script>
 
 <template>
   <main>
+
+    <CabecalhoComp />
+    <RouterView />
     <div class="carousel-section">
       <div id="carouselExampleAutoplaying" class="carousel main" data-bs-ride="carousel">
         <div class="carousel-inner">
@@ -17,41 +22,46 @@
             <img src="@/assets/imagens/1.jpg" class="d-block w-100" alt="" />
           </div>
         </div>
-        <button
-          class="carousel-control-prev styled-button"
-          type="button"
-          data-bs-target="#carouselExampleAutoplaying"
-          data-bs-slide="prev"
-        >
+        <button class="carousel-control-prev styled-button" type="button" data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
-        <button
-          class="carousel-control-next styled-button"
-          type="button"
-          data-bs-target="#carouselExampleAutoplaying"
-          data-bs-slide="next"
-        >
+        <button class="carousel-control-next styled-button" type="button" data-bs-target="#carouselExampleAutoplaying"
+          data-bs-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
         </button>
       </div>
-      <p class="carousel-desc">Explore nosso portfólio diversificado e descubra a qualidade que podemos trazer ao seu evento.</p>
+      <p class="carousel-desc">Explore nosso portfólio diversificado e descubra a qualidade que podemos trazer ao seu
+        evento.</p>
     </div>
 
     <div class="container">
       <h2 class="section-title">Destaques</h2>
       <div class="additional-container">
-        <div class="additional-item"><ImagemComp /></div>
-        <div class="additional-item"><ImagemComp /></div>
-        <div class="additional-item"><ImagemComp /></div>
+        <div class="additional-item">
+          <ImagemComp />
+        </div>
+        <div class="additional-item">
+          <ImagemComp />
+        </div>
+        <div class="additional-item">
+          <ImagemComp />
+        </div>
       </div>
 
       <h2 class="section-title">Recentes</h2>
       <div class="xd-container">
-        <div class="bottom-i"><ImagemComp /></div>
-        <div class="bottom-i"><ImagemComp /></div>
-        <div class="bottom-i"><ImagemComp /></div>
+        <div class="bottom-i">
+          <ImagemComp />
+        </div>
+        <div class="bottom-i">
+          <ImagemComp />
+        </div>
+        <div class="bottom-i">
+          <ImagemComp />
+        </div>
       </div>
 
       <h2 class="section-title">Depoimentos</h2>
@@ -62,115 +72,117 @@
       </div>
     </div>
   </main>
+  <FooterComp />
 </template>
 
 
 <style scoped>
-  @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700&display=swap');
 
-  * {
-    font-family: 'Raleway', sans-serif;
-  }
+* {
+  font-family: 'Raleway', sans-serif;
+}
 
-  .container {
-    max-width: none;
-    width: 100%;
-    background-color: #fff;
-  }
+.container {
+  max-width: none;
+  width: 100%;
+  background-color: #fff;
+}
 
-  .additional-container {
-    display: flex;
-    justify-content: space-between;
-    margin-top: 20px;
-  }
+.additional-container {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+}
 
-  .additional-item {
-    width: calc(33.33% - 10px);
-    padding: 10px;
-    text-align: center;
-  }
+.additional-item {
+  width: calc(33.33% - 10px);
+  padding: 10px;
+  text-align: center;
+}
 
-  .bottom-container {
-    display: flex;
-    justify-content: space-between;
-    margin-top: 20px;
-  }
+.bottom-container {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+}
 
-  .bottom-item {
-    width: 220px;
-    border: 1px solid #ccd5ae;
-    padding: 10px;
-    text-align: center;
-    overflow: hidden;
-    display: flex;
-  }
+.bottom-item {
+  width: 220px;
+  border: 1px solid #ccd5ae;
+  padding: 10px;
+  text-align: center;
+  overflow: hidden;
+  display: flex;
+}
 
-  .bottom-item img {
-    width: 220px;
-  }
+.bottom-item img {
+  width: 220px;
+}
 
-  .xd-container {
-    display: flex;
-    justify-content: space-between;
-    margin-top: 20px;
-  }
+.xd-container {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+}
 
-  .bottom-i {
-    width: calc(33.33% - 10px);
-    padding: 10px;
-    text-align: center;
-  }
+.bottom-i {
+  width: calc(33.33% - 10px);
+  padding: 10px;
+  text-align: center;
+}
 
-  .carousel-inner {
-    height: 300px;
-  }
+.carousel-inner {
+  height: 300px;
+}
 
-  .carousel-inner img {
-    height: 100%;
-    object-fit: cover;
-  }
+.carousel-inner img {
+  height: 100%;
+  object-fit: cover;
+}
 
-  .carousel-section {
-    text-align: center;
-  }
+.carousel-section {
+  text-align: center;
+}
 
-  .carousel-desc {
-    margin-top: 10px;
-    color: #555;
-  }
+.carousel-desc {
+  margin-top: 10px;
+  color: #555;
+}
 
-  .section-title {
-    text-align: center;
-    margin-top: 20px;
-    font-weight: bold;
-  }
+.section-title {
+  text-align: center;
+  margin-top: 20px;
+  font-weight: bold;
+}
 
-  .testimonials {
-    display: flex;
-    justify-content: space-between;
-    padding: 20px;
-    background-color: #FFF;
-    border-radius: 8px;
-  }
+.testimonials {
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  background-color: #FFF;
+  border-radius: 8px;
+}
 
-  blockquote {
-    font-size: 18px;
-    font-style: italic;
-    padding: 10px;
-    border-left: 4px solid #ccd5ae;
-    margin: 10px;
-  }
+blockquote {
+  font-size: 18px;
+  font-style: italic;
+  padding: 10px;
+  border-left: 4px solid #ccd5ae;
+  margin: 10px;
+}
 
-  .styled-button {
-    background-color: #ccd5ae;
-    color: white;
-    padding: 10px;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    transition: background-color 0.3s ease;
-  }
-  .styled-button:hover {
-    background-color: #ccd5ae;
-  }
+.styled-button {
+  background-color: #ccd5ae;
+  color: white;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.styled-button:hover {
+  background-color: #ccd5ae;
+}
 </style>
